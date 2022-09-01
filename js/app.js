d50c6215d7a2fa291914f794ccd9b6c4dc636d7a
trmorningstar <trmorningstar@gmail.com>
2022-09-01 16:41:46 -0400

    Complete step 2

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
 
 
-
 /*---------------------------- Variables (state) ----------------------------*/
-
-
-
+board, turn, winner
 /*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelector(".board > div")
+console.log(squareEls)
 
-
-
+const messageEl = document.querySelector("message")
+console.log(messageEl)
 /*----------------------------- Event Listeners -----------------------------*/
 
 
